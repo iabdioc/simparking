9c2b12c23fcb14ab487e933dbe288e23dde58ee6
joan <iabd@ioc.cat>
2024-02-15 19:32:17 +0100

    240215_1932

--- clusteringpytorch/clusteringpytorch.py
+++ clusteringpytorch/clusteringpytorch.py
@@ -9,10 +9,12 @@ from contextlib import contextmanager, redirect_stderr, redirect_stdout
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import torch
+import numpy as np
 
 from sklearn import preprocessing
-from sklearn.cluster import KMeans
 from sklearn.metrics.cluster import homogeneity_score, completeness_score, v_measure_score
+from kmeans_pytorch import kmeans, kmeans_predict
 
 @contextmanager
 def suppress_stdout_stderr():
@@ -131,25 +133,28 @@ def visualitzar_pairplot(df):
 	plt.savefig("img/pairplot.png")
 	#plt.show()
 
-def clustering_kmeans(data):
+def clustering_kmeans(data, dev):
 	"""
 	Crea el model KMeans de sk-learn, amb 4 clusters (estem cercant 4 agrupacions)
 	Entrena el model
 
 	arguments:
 		data -- el dataframe amb les dades seleccionades (en principi totes perquè hem eliminat les que no interessen)
+		dev -- device (cpu o gpu)
 
-	Returns: model (objecte KMeans)
+	Returns: clusters (tupla de labels del dataset, i centers)
 	"""
-	#random_state és important si vull reproduir els resultats (sempre s'assignen els mateixos labels als mateixos grups)
-	model = KMeans(n_clusters=4, random_state=42)
+	# a pytorch treballem amb tensors
+	tensor = torch.tensor(data.values)
+	logging.debug('\nTensor:\n%s\n...', tensor[:3])
 
-	with suppress_stdout_stderr():
-		model.fit(data)
+	cluster_ids_x, cluster_centers = kmeans(
+		X=tensor, num_clusters=4, distance='euclidean', device=device
+	)
 
-	return model
+	return cluster_ids_x, cluster_centers
 
-def visualitzar_clusters(data, labels):
+def visualitzar_clusters(data, labels, centers):
 	"""
 	Visualitza els clusters en diferents colors. Probem diferents combinacions de parells d'atributs
 
@@ -160,31 +165,42 @@ def visualitzar_clusters(data, labels):
 	Returns: None
 	"""
 	# Visualitzar els clústers/agrupaments
+	tensor = torch.tensor(data.values)
 
-	try:
-		os.makedirs(os.path.dirname('img/'))
-	except FileExistsError:
-		pass
-
-	fig = plt.figure()
-	sns.scatterplot(x='dia_setmana_dec', y='hora', data=data, hue=labels, palette="rainbow")
+	plt.figure(figsize=(4, 3), dpi=160)
+	plt.scatter(tensor[:, 2], tensor[:, 3], c=cluster_ids_x, s=1)
+	for num_cluster in range(len(centers)):
+		plt.scatter(cluster_centers[num_cluster, 2], cluster_centers[num_cluster, 3], label=num_cluster, edgecolors='black')
+	plt.legend()
+	plt.xlabel("dia setmana")
+	plt.ylabel("count")
 	plt.savefig("img/grafica1.png")
-	fig.clf()
 	#plt.show()
+	plt.clf()
 
-	fig = plt.figure()
-	sns.scatterplot(x='hora', y='durada', data=data, hue=labels, palette="rainbow")
+	plt.figure(figsize=(4, 3), dpi=160)
+	plt.scatter(tensor[:, 1], tensor[:, 0], c=cluster_ids_x, s=1)
+	for num_cluster in range(len(cluster_centers)):
+		plt.scatter(cluster_centers[num_cluster, 1], cluster_centers[num_cluster, 0], label=num_cluster, edgecolors='black')
+	plt.legend()
+	plt.xlabel("hora")
+	plt.ylabel("durada")
 	plt.savefig("img/grafica2.png")
-	fig.clf()
 	#plt.show()
+	plt.clf()
 
-	fig = plt.figure()
-	sns.scatterplot(x='hora', y='count', data=data, hue=labels, palette="rainbow")
+	plt.figure(figsize=(4, 3), dpi=160)
+	plt.scatter(tensor[:, 1], tensor[:, 3], c=cluster_ids_x, s=1)
+	for num_cluster in range(len(cluster_centers)):
+		plt.scatter(cluster_centers[num_cluster, 1], cluster_centers[num_cluster, 3], label=num_cluster, edgecolors='black')
+	plt.legend()
+	plt.xlabel("hora")
+	plt.ylabel("count")
 	plt.savefig("img/grafica3.png")
-	fig.clf()
 	#plt.show()
+	plt.clf()
 
-def associar_clusters_patrons(tipus, model):
+def associar_clusters_patrons(tipus, centers):
 	"""
 	Associa els clústers (labels 0, 1, 2, 3) als patrons de comportament (I, II, III, IV).
 	S'han trobat 4 clústers però aquesta associació encara no s'ha fet.
@@ -200,7 +216,7 @@ def associar_clusters_patrons(tipus, model):
 
 	logging.debug('\nCentres:')
 	for j in range(len(tipus)):
-		logging.debug('\n{:d}:\t(durada: {:.1f}\thora: {:.1f}\tdia_setmana_dec: {:.1f}\tcount: {:.1f})'.format(j, model.cluster_centers_[j][dicc['durada']], model.cluster_centers_[j][dicc['hora']], model.cluster_centers_[j][dicc['dia_setmana_dec']], model.cluster_centers_[j][dicc['count']]))
+		logging.debug('\n{:d}:\t(durada: {:.1f}\thora: {:.1f}\tdia_setmana_dec: {:.1f}\tcount: {:.1f})'.format(j, centers[j][dicc['durada']], centers[j][dicc['hora']], centers[j][dicc['dia_setmana_dec']], centers[j][dicc['count']]))
 
 	# Procés d'assignació
 	ind_label_0 = -1
@@ -212,11 +228,10 @@ def associar_clusters_patrons(tipus, model):
 	val_count_max = -1
 	val_count_min = 1000
 
-	for j, center in enumerate(clustering_model.cluster_centers_):
-
-		v_hora = round(center[dicc['hora']], 1)
-		v_durada = round(center[dicc['durada']], 1)
-		v_count = round(center[dicc['count']], 1)
+	for j, center in enumerate(centers):
+		v_hora = round(center[dicc['hora']].item(), 1)
+		v_durada = round(center[dicc['durada']].item(), 1)
+		v_count = round(center[dicc['count']].item(), 1)
 
 		if (v_hora) > val_hora_max:
 			ind_label_0 = j
@@ -228,7 +243,7 @@ def associar_clusters_patrons(tipus, model):
 			ind_label_3 = j
 			val_count_min = v_count
 		#if (v_hora < 0 and v_durada > 0 and v_count < 0):
-		#	ind_label_1 = j
+		# ind_label_1 = j
 
 	lst = [0, 1, 2, 3]
 	lst.remove(ind_label_0)
@@ -242,13 +257,14 @@ def associar_clusters_patrons(tipus, model):
 
 	return tipus
 
-def nova_prediccio(dades, sc, model):
+def nova_prediccio(dades, sc, cluster_centers):
 	"""
 	Passem nous valors de registre en el parking, per tal d'assignar aquests valors a un dels 4 clústers
 
 	arguments:
 		dades -- array de Python, que segueix l'estructura 'matricula', 'durada', 'hora', 'dia_setmana_dec'
-
+		sc -- objecte Scaler
+		cluster_centers -- objecte cluster_centers
 	Returns: (dades agrupades, prediccions del model)
 	"""
 	df_dades_cotxe = pd.DataFrame(columns=['matricula', 'durada', 'hora', 'dia_setmana_dec'], data=dades)
@@ -256,9 +272,14 @@ def nova_prediccio(dades, sc, model):
 	df_dades_cotxe_gb = group_by(df_dades_cotxe)
 	df_dades_cotxe_gb_norm = normalitzacio(df_dades_cotxe_gb, sc)
 
-	logging.info('\nNous valors agrupats i normalitzats:\n%s', df_dades_cotxe_gb_norm[:3])
+	logging.debug('\nNous valors agrupats i normalitzats:\n%s', df_dades_cotxe_gb_norm[:3])
+	tensor_cotxe = torch.tensor(df_dades_cotxe_gb_norm.values)
 
-	return df_dades_cotxe_gb, model.predict(df_dades_cotxe_gb_norm)
+	cluster_ids_y = kmeans_predict(
+		tensor_cotxe, cluster_centers, 'euclidean', device=device
+	)
+
+	return df_dades_cotxe_gb, cluster_ids_y
 
 def generar_informes(df, tipus):
 	"""
@@ -299,7 +320,7 @@ def generar_informes(df, tipus):
 
 if __name__ == "__main__":
 
-	logging.basicConfig(format='%(message)s', level=logging.DEBUG) # canviar entre DEBUG i INFO
+	logging.basicConfig(format='%(message)s', level=logging.INFO) # canviar entre DEBUG i INFO
 	logging.getLogger('matplotlib.font_manager').setLevel(logging.ERROR) # per tal de què el matplotlib no vomiti molts missatges
 
 	path_dataset = '../simulaciodades/data/registre_durada.csv'
@@ -324,23 +345,29 @@ if __name__ == "__main__":
 	selected_data = parking_data_gb_norm.iloc[:, 0:4]
 	logging.debug('\nDades per l\'entrenament:\n%s\n...', selected_data[:3])
 
-	clustering_model = clustering_kmeans(selected_data)
-	data_labels = clustering_model.labels_
+	# set device
+	if torch.cuda.is_available():
+		device = torch.device('cuda:0')
+	else:
+		device = torch.device('cpu')
 
-	logging.info('\nHomogeneity: %.3f', homogeneity_score(true_labels, data_labels))
-	logging.info('Completeness: %.3f', completeness_score(true_labels, data_labels))
-	logging.info('V-measure: %.3f', v_measure_score(true_labels, data_labels))
+	cluster_ids_x, cluster_centers = clustering_kmeans(selected_data, device)
 
-	visualitzar_clusters(selected_data, data_labels)
+	logging.info('\nHomogeneity: %.3f', homogeneity_score(true_labels, cluster_ids_x))
+	logging.info('Completeness: %.3f', completeness_score(true_labels, cluster_ids_x))
+	logging.info('V-measure: %.3f', v_measure_score(true_labels, cluster_ids_x))
+
+	visualitzar_clusters(selected_data, cluster_ids_x, cluster_centers)
 
 	# array de diccionaris que assignarà els tipus als labels
 	tipus = [{'name': 'tipus I'}, {'name': 'tipus II'}, {'name': 'tipus III'}, {'name': 'tipus IV'}]
 
 	# afegim la columna label al dataframe
-	parking_data_gb['label'] = clustering_model.labels_.tolist()
+	parking_data_gb['label'] = cluster_ids_x.tolist()
+
 	logging.debug('\nColumna label:\n%s', parking_data_gb[:5])
 
-	tipus = associar_clusters_patrons(tipus, clustering_model)
+	tipus = associar_clusters_patrons(tipus, cluster_centers)
 	logging.info('\nTipus i labels:\n%s', tipus)
 
 	# Classificació de nous valors
@@ -356,16 +383,17 @@ if __name__ == "__main__":
 		['3560 ROQ', 13461, 20.1, 6.8],
 		['3560 ROQ', 13818, 19.4, 5.8],
 		['3560 ROQ', 11251, 19.9, 6.8]
-	]*2
+	]*8
 
-	logging.debug('\nNous valors:\n%s', dades_cotxe)
-	dades_cotxe_gb, pred = nova_prediccio(dades_cotxe, scaler, clustering_model)
+	logging.debug('\nNous valors:\n%s', dades_cotxe[:5])
+	dades_cotxe_gb, pred = nova_prediccio(dades_cotxe, scaler, cluster_centers)
+	# NOTA: a pytorch l'assignació dels dos cotxes als tipus I i II no sempre es fa de forma correcta (la majoria de les vegades sí. Fixar-se en el valor que dóna es scores)
 	logging.info('\nPredicció dels valors:\n%s', pred)
 
 	#Assignació dels nous valors als tipus
 	for i, p in enumerate(pred):
 		t = [t for t in tipus if t['label'] == p]
-    logging.info('%s: %s', dades_cotxe_gb.index[i], t[0]['name'])
+		logging.info('%s: %s', dades_cotxe_gb.index[i], t[0]['name'])
 
 	# Generació d'informes
 	generar_informes(parking_data_gb, tipus)
